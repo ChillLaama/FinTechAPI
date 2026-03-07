69a385c5e45ad4c1c551d2ca787cac3442b757b7
rudansd <rudansd@stahls.com>
2026-03-07 10:52:34 +0200

    refactor: old project files removal

--- FinTechAPI/Models/Account/AccountDto.cs
+++
@@ -1,8 +0,0 @@
-namespace FinTechAPI.Models;
-
-public class AccountDto
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public decimal Balance { get; set; }
-}
--- FinTechAPI/Models/Transaction/TransactionType.cs
+++
@@ -1,8 +0,0 @@
-namespace FinTechAPI.Models;
-
-public enum TransactionType
-{
-    Income,
-    Expense,
-    Transfer
-}
